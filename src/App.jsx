9fba64bb7ec5888dd015a9e2ccad2424f39b03f2
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-05-28 14:40:19 -0400

    updated importts and routes in app.jsx... services now include luxury glass cards elegant hover effects responsive service grid gold premium accents high-end section transitions

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Services from './components/Services'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
 
       <Navbar />
       <Hero />
+      <Services />
 
     </>
 
