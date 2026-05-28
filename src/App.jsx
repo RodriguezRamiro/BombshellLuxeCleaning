ced418a2a0be871eedca2c918a81bf34f61ecf1a
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-05-28 14:27:04 -0400

    Initial Vite project setup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+
+  return (
+
+    <>
+
+      <h1
+        style={{
+          height:'100vh',
+          display:'flex',
+          justifyContent:'center',
+          alignItems:'center',
+          fontSize:'4rem'
+        }}
+      >
+        Bombshell Luxe Cleaning
+      </h1>
+
+    </>
+
+  )
+
+}
+
+export default App
