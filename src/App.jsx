e63c0bdec9f476de13d445c6d117c889934ead24
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-05-28 14:47:34 -0400

    included transformations import. transofmation now includes premium before/after gallery cinematic transformation section luxury hover interactions visual emotional proof upscale presentation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Services from './components/Services'
+import Transformations from './components/Transformations'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Hero />
       <Services />
+      <Transformations />
 
     </>
 
