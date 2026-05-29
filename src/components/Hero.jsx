dc180c42f5aaec83cacd1991bb8af3e13ff60f28
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-05-29 16:07:39 -0400

    installed famer motion library. upgraded hero entrance by importing framer-motion, added motion.div to improve premium feel no fast timeing, no dramatic flaot and easeout curve

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@
 
 import '../styles/hero.css'
 import Particles from './Particles'
+import { motion } from 'framer-motion'
 
 function Hero() {
 
@@ -13,7 +14,23 @@ function Hero() {
 
       <div className="hero-overlay"></div>
 
-      <div className="hero-content">
+    <motion.div
+      className="hero-content"
+      initial={{
+      opacity:0,
+      y:40
+    }}
+
+      animate={{
+      opacity:1,
+      y:0
+    }}
+
+      transition={{
+      duration:0.9,
+      ease:'easeOut'
+    }}
+  >
 
         <p className="hero-subtitle">
           Tampa Bay Luxury Cleaning Services
@@ -44,7 +61,7 @@ function Hero() {
 
         </div>
 
-      </div>
+      </motion.div>
 
     </section>
 
