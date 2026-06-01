9f112d875e640573df1139d6344aff0f3246dd67
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-06-01 13:40:49 -0400

    cleaned up migrated code from component.

--- src/components/Transformations.jsx
+++ src/components/Transformations.jsx
@@ -2,53 +2,9 @@
 
 import '../styles/transformations.css'
 import Reveal from './Reveal'
-
-import before1 from '../assets/before1.jpeg'
-import after1 from '../assets/after1.jpeg'
-
-import before2 from '../assets/before2.jpeg'
-import after2 from '../assets/after2.jpeg'
-
-import before3 from '../assets/before3.jpg'
-import after3 from '../assets/after3.jpg'
-
-const transformations = [
-
-  {
-    before: before1,
-    after: after1
-  },
-
-  {
-    before: before2,
-    after: after2
-  },
-
-  {
-    before: before3,
-    after: after3
-  }
-
-]
+import { transformations } from '../data/transformationsData'
 
 
-/*
-
-user needs a way to submit new before and afters as needed.
-current method ramirorodriguezalvarez@RAMIROs-MacBook-Pro bomshellLuxeCleaning % cd src
-ramirorodriguezalvarez@RAMIROs-MacBook-Pro src % ls
-App.jsx         components      main.jsx        styles
-assets          data            sections
-ramirorodriguezalvarez@RAMIROs-MacBook-Pro src % cd assets
-ramirorodriguezalvarez@RAMIROs-MacBook-Pro assets % ls
-after1.jpeg     before1.jpeg    promo1.jpg
-after2.jpeg     before2.jpeg    promo2.jpg
-after3.jpg      before3.jpg     promo3.jpg
-ramirorodriguezalvarez@RAMIROs-MacBook-Pro assets %
-images need to pop up to show full image both on mobile and desktop maybe like a zoom in pop up
-maybe modal? not sure how to do that.
-
-*/
 function Transformations() {
 
   return (
