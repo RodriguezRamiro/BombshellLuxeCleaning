23e75f43bc1d3e83548fc50f8e56591738a6eedf
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-06-04 07:19:52 -0400

    refactored testimonials stats migrating stats array to its own file.

---
+++ src/data/statsData.js
@@ -0,0 +1,23 @@
+/* //BomshellLuxeCleaning/src/data/statsData.js */
+
+export const stats = [
+
+    {
+      id:'luxury-cleanings',
+      value:'250+',
+      label:'Luxury Cleanings'
+    },
+
+    {
+      id:'recurring-clients',
+      value:'98%',
+      label:'Recurring Clients'
+    },
+
+    {
+      id:'five-star',
+      value:'5-Star',
+      label:'Client Experience'
+    }
+
+  ]
