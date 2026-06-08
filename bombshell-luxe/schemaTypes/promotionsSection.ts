bd7271586f6c0596e2ab88360972378211a12e53
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-06-08 13:57:37 -0400

    Created promotions schema. stores title, description, image, button label and link.

---
+++ bombshell-luxe/schemaTypes/promotionsSection.ts
@@ -0,0 +1,25 @@
+/* //BomshellLuxeCleaning/bombshell-luxe/schemaTypes/promotionsSection.ts */
+
+import { defineField, defineType } from 'sanity'
+
+export const promotionsSectionType = defineType({
+  name: 'promotionsSection',
+  title: 'Promotions Section',
+  type: 'document',
+
+  fields: [
+
+    defineField({
+      name: 'sectionSubtitle',
+      title: 'Section Subtitle',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'sectionTitle',
+      title: 'Section Title',
+      type: 'string',
+    }),
+
+  ],
+})
