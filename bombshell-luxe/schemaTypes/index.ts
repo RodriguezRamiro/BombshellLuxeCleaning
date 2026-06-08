c84660af6a58c8b104cd08b44d1199621b406028
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-06-08 06:54:34 -0400

    Registerd schemas inside index.ts

--- bombshell-luxe/schemaTypes/index.ts
+++ bombshell-luxe/schemaTypes/index.ts
@@ -1,7 +1,11 @@
 import {heroType} from './hero'
 import { servicesType } from './services'
+import { testimonialsSectionType } from './testimonialsSection'
+import { testimonialType } from './testimonial'
 
 export const schemaTypes = [
     heroType,
-    servicesType
+    servicesType,
+    testimonialsSectionType,
+    testimonialType
 ]
