f9537e14e20182469d796b806f71827386fbbf77
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-06-08 13:40:14 -0400

    registered schema in index.ts, with array variables and imports.

--- bombshell-luxe/schemaTypes/index.ts
+++ bombshell-luxe/schemaTypes/index.ts
@@ -2,10 +2,16 @@ import {heroType} from './hero'
 import { servicesType } from './services'
 import { testimonialsSectionType } from './testimonialsSection'
 import { testimonialType } from './testimonials'
+import { transformationsSectionType } from './transformationsSection'
+import { transformationType } from './transformation'
+
+
 
 export const schemaTypes = [
-    heroType,
-    servicesType,
-    testimonialsSectionType,
-    testimonialType
+  heroType,
+  servicesType,
+  testimonialsSectionType,
+  testimonialType,
+  transformationsSectionType,
+  transformationType
 ]
