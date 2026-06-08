e7e8d5d04f46c634076c51844d3b2ee0beb877dc
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-06-08 13:59:16 -0400

    created schema description object

---
+++ bombshell-luxe/schemaTypes/promotion.ts
@@ -0,0 +1,54 @@
+/* //BomshellLuxeCleaning/bombshell-luxe/schemaTypes/promotion.ts */
+
+
+import { defineField, defineType } from 'sanity'
+
+export const promotionType = defineType({
+  name: 'promotion',
+  title: 'Promotion',
+  type: 'document',
+
+  fields: [
+
+    defineField({
+      name: 'title',
+      title: 'Promotion Title',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'description',
+      title: 'Promotion Description',
+      type: 'text',
+    }),
+
+    defineField({
+      name: 'buttonText',
+      title: 'Button Text',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'buttonLink',
+      title: 'Button Link',
+      type: 'url',
+    }),
+
+    defineField({
+      name: 'image',
+      title: 'Promotion Image',
+      type: 'image',
+
+      options: {
+        hotspot: true,
+      },
+    }),
+
+    defineField({
+      name: 'imageAlt',
+      title: 'Image Alt Text',
+      type: 'string',
+    }),
+
+  ],
+})
