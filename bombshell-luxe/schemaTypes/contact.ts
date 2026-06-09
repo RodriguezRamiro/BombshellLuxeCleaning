fa49bdd95bda1ea22cd3edbb09dd26f11faae39d
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-06-09 06:36:59 -0400

    created contacts.ts schema

---
+++ bombshell-luxe/schemaTypes/contact.ts
@@ -0,0 +1,62 @@
+/* //BomshellLuxeCleaning/bombshell-luxe/schemaTypes/contact.ts */
+
+
+import { defineField, defineType } from 'sanity'
+
+export const contactType = defineType({
+  name: 'contact',
+  title: 'Contact Section',
+  type: 'document',
+
+  fields: [
+
+    defineField({
+      name: 'label',
+      title: 'Contact Label',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'heading',
+      title: 'Heading',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+
+    defineField({
+      name: 'phone',
+      title: 'Phone Number',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'email',
+      title: 'Email Address',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'location',
+      title: 'Service Areas',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'formEndpoint',
+      title: 'FormSpree Endpoint',
+      type: 'url',
+    }),
+
+    defineField({
+      name: 'buttonText',
+      title: 'Submit Button Text',
+      type: 'string',
+    }),
+
+  ],
+})
