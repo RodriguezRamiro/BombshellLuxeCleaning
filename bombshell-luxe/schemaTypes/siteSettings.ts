267f289bf873a65d4d07434efdeb574369a9b6d8
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-06-09 06:55:21 -0400

    created sitesettings ts to handle global settings.

---
+++ bombshell-luxe/schemaTypes/siteSettings.ts
@@ -0,0 +1,78 @@
+/* //BomshellLuxeCleaning/bombshell-luxe/schemaTypes/siteSettings.ts */
+
+
+import { defineField, defineType } from 'sanity'
+
+export const siteSettingsType = defineType({
+  name: 'siteSettings',
+  title: 'Site Settings',
+  type: 'document',
+
+  fields: [
+
+    defineField({
+      name: 'businessName',
+      title: 'Business Name',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'navButtonText',
+      title: 'Navbar Button Text',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'navButtonLink',
+      title: 'Navbar Button Link',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'navigationLinks',
+      title: 'Navigation Links',
+      type: 'array',
+
+      of: [
+        {
+          type: 'object',
+
+          fields: [
+
+            defineField({
+              name: 'label',
+              title: 'Link Label',
+              type: 'string',
+            }),
+
+            defineField({
+              name: 'href',
+              title: 'Anchor Link',
+              type: 'string',
+            }),
+
+          ],
+        },
+      ],
+    }),
+
+    defineField({
+      name: 'footerText',
+      title: 'Footer Text',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'instagramUrl',
+      title: 'Instagram URL',
+      type: 'url',
+    }),
+
+    defineField({
+      name: 'facebookUrl',
+      title: 'Facebook URL',
+      type: 'url',
+    }),
+
+  ],
+})
